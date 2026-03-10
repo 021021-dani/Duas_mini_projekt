c144b331b515a9457d5aa649307bf4d7f1a22a34
021021-dani <danielparhizkari@gmail.com>
2026-03-10 21:16:31 +0100

    loading KD images

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import numpy as np
 
 def main():
     
-    print("+-------------------------------+")
-    print("| King Domino points calculator |")
-    print("+-------------------------------+")
-    
+    print("\n===================================")
+    print("     KING DOMINO BOARD ANALYZER")
+    print("===================================\n")
+
     print("\n======= Loadin KD Images =====")
-    folder = r"/Users/mamali/Desktop/Ai_aau/Projekter/Mini_projekt2/KingDominoData"
-    for file_name in sorted(os.listdir(folder), key=lambda x: int(x.split(".")[0])): # Ordner Rækkefølge af billeder
+    
+    dataset_folder = r"/Users/mamali/Desktop/Ai_aau/Projekter/Mini_projekt2/KingDominoData"
+    for file_name in sorted(os.listdir(dataset_folder), key=lambda x: int(x.split(".")[0])): # Ordner Rækkefølge af billeder
         if file_name.lower().endswith(".jpg"):
-            image_path = os.path.join(folder, file_name)
+            image_name = os.path.join(dataset_folder, file_name)
             print("Processing:", file_name)
             
-            if not os.path.isfile(image_path):
+            if not os.path.isfile(image_name):
                 print("Image not found")
-            image = cv.imread(image_path)
+            image = cv.imread(image_name)
             
 if __name__ =="__main__":
     main() 
+
