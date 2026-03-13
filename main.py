555b603095cae47308e021ff2785fe4d080cfac7
021021-dani <danielparhizkari@gmail.com>
2026-03-13 13:08:15 +0100

    Loading Image

--- main.py
+++ main.py
@@ -1,33 +1,30 @@
 import cv2 as cv
 import os
 
-import os
-import cv2 as cv
-
 def main():
 
     print("\n======= Loading KD Images =====")
-    
-    dataset_folder = r"/Users/mamali/Desktop/Ai_aau/Projekter/Mini_projekt2/Duas_mini_projekt/KingDominoData"
-    
+
+    dataset_folder = r"/Users/mamali/Desktop/Ai_aau/Projekter/Duas_mini_projekt/KingDominoData"
 
     files = [
         f for f in os.listdir(dataset_folder)
         if f.lower().endswith(".jpg") and f.split(".")[0].isdigit()
     ]
-
-    # Sorter efter tallet i filnavnet
+    
     files = sorted(files, key=lambda x: int(x.split(".")[0]))
 
     for file_name in files:
-        image_name = os.path.join(dataset_folder, file_name)
-        print("Processing:", file_name)
 
-        if not os.path.isfile(image_name):
+        image_path = os.path.join(dataset_folder, file_name)
+
+        print("Loading image:", file_name)
+
+        if not os.path.isfile(image_path):
             print("Image not found")
             continue
 
-        image = cv.imread(image_name)
+        image = cv.imread(image_path)
 
         if image is None:
             print("Could not load image:", file_name)
@@ -35,4 +32,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
--- perspektivkorrig.py
+++
@@ -1,5 +0,0 @@
-"""
-Module: perspektivkorrig.py
-Responsibility: Detect the board, correct perspective, and slice into a 5x5 2D grid.
-"""
-
